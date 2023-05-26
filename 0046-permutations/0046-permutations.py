d13b3b06b79555b54f8be00ad8c34426bf2118f8
Arun Maramraj <imarun500@gmail.com>
2023-05-26 18:44:20 +0530

    Time: 58 ms (20.46%), Space: 16.6 MB (14.57%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,21 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        
+        
+        output = []
+        temp = nums
+        
+        def compute(i):
+            
+            if (i>= len(nums)):
+                output.append(temp.copy())
+                return
+            
+            for j in range (i , len(nums)):
+                temp[i] , temp[j] = temp[j] , temp[i]
+                compute(i+1)
+                temp[i] , temp[j] = temp[j] , temp[i]
+        
+        compute(0)
+        return output        
+                
