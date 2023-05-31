cf00b986a52ad9553d7c163310d8b425a33e4a21
Arun Maramraj <imarun500@gmail.com>
2023-05-31 19:33:43 +0530

    Time: 100 ms (5.98%), Space: 16.6 MB (19.44%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -7,8 +7,9 @@ class Solution:
         
         vis = set()
         
+        tempcolor = image[sr][sc]
         
-        def dfs(i,j,tempcolor):
+        def dfs(i,j):
             
             if (i<0 or j<0 or i>=m or j>=n or (i,j) in vis or image[i][j]!=tempcolor ):
                 return
@@ -16,15 +17,15 @@ class Solution:
             
             vis.add((i,j))
             
-            dfs(i+1,j , image[i][j])
-            dfs(i,j+1 , image[i][j])
-            dfs(i,j-1 , image[i][j])
-            dfs(i-1,j , image[i][j])
+            dfs(i+1,j)
+            dfs(i,j+1 )
+            dfs(i,j-1 )
+            dfs(i-1,j )
             
             image[i][j] = color
         
         
-        dfs(sr,sc,image[sr][sc])
+        dfs(sr,sc)
         
         return image
             
