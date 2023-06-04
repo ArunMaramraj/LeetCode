90a5693a1bb77020d81238999cb3eac6d8e646b5
Arun Maramraj <imarun500@gmail.com>
2023-06-04 20:35:47 +0530

    Time: 600 ms (94.45%), Space: 79.1 MB (50.14%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -0,0 +1,40 @@
+class Solution:
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        
+        m = len(grid)
+        n = len(grid[0])
+
+        def dfs(i, j):
+            if i < 0 or i >= m or j < 0 or j >= n or grid[i][j] == 0:
+                return
+
+            grid[i][j] = 0  # Mark the cell as visited
+
+            # Recursively explore the adjacent cells
+            dfs(i+1, j)
+            dfs(i-1, j)
+            dfs(i, j+1)
+            dfs(i, j-1)
+
+        # Traverse the boundary and mark all reachable land cells
+        for i in range(m):
+            dfs(i, 0)
+            dfs(i, n-1)
+
+        for j in range(n):
+            dfs(0, j)
+            dfs(m-1, j)
+
+        # Count the remaining land cells
+        count = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    count += 1
+
+        return count
+
+
+
+        
+          
