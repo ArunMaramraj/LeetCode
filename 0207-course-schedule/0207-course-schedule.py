727b6a39305f15e0cd8a53c1fda18c88170a61d7
Arun Maramraj <imarun500@gmail.com>
2023-06-06 19:51:53 +0530

    Time: 116 ms (43.51%), Space: 17.7 MB (67.38%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,26 +1,44 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        queue = []
-        indegree = [0] * numCourses
-        topo = []
-        
-        adj = [[] for _ in range(numCourses)]
-        
-        for i in prerequisites:
-            adj[i[0]].append(i[1])
-            indegree[i[1]] += 1
-        
-        for i in range(numCourses):
-            if indegree[i] == 0:
-                queue.append(i)
-        
-        while queue:
-            course = queue.pop(0)
-            topo.append(course)
             
-            for neighbor in adj[course]:
-                indegree[neighbor] -= 1
-                if indegree[neighbor] == 0:
-                    queue.append(neighbor)
         
-        return len(topo) == numCourses
+            
+            
+            queue = []
+            indegree = [0] * numCourses
+            topo = []
+            V = len(prerequisites)
+            
+            adj = [[] for _ in range(numCourses)]
+
+            
+            for i in prerequisites:
+                adj[i[0]].append(i[1])
+            
+
+            for i in range(numCourses):     
+                for j in adj[i]:
+                    indegree[j]+=1
+
+            for i in range (len(indegree)):     
+                if indegree[i]==0 :                
+                    queue.append(i) 
+
+
+            while(len(queue)!=0):
+                ele = queue.pop(0)
+                topo.append(ele)
+
+                for j in adj[ele]:
+                    indegree[j]-=1
+                    if(indegree[j]==0):
+                        queue.append(j)
+            
+            
+            
+            if len(topo)<numCourses : return False
+            else: return True
+        
+        
+        
+        
