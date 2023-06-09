46f4458f9f1d3d238545297b4511e4680eae1295
Arun Maramraj <imarun500@gmail.com>
2023-06-09 22:48:54 +0530

    Time: 1117 ms (5.04%), Space: 17.4 MB (9.19%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -0,0 +1,37 @@
+from collections import defaultdict
+
+class Solution:
+    def findRedundantConnection(self, edges):
+        adj = defaultdict(list)
+
+        for edge in edges:
+            u, v = edge
+            adj[v].append(u)
+            adj[u].append(v)
+
+        output1 = set()
+        output2 = []
+
+        def dfs(i, parent, visited):
+            visited.add(i)
+
+            for j in adj[i]:
+                if j == parent:
+                    continue
+                if j in visited:
+                    output1.add((i, j))
+                else:
+                    dfs(j, i, visited)
+
+        for u, v in edges:
+            visited = set()
+            if u not in adj or v not in adj:
+                continue
+            dfs(u, -1, visited)
+
+        for sets in output1:
+            output2.append(list(sets))
+
+        intersection = [x for x in edges if x in output2]
+
+        return intersection[-1] if intersection else []
