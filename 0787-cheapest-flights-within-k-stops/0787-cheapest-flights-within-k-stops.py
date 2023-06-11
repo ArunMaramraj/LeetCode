7b5247faaa16de1f5558a0a1d57eeabda78153fd
Arun Maramraj <imarun500@gmail.com>
2023-06-11 08:23:55 +0530

    Time: 124 ms (64.29%), Space: 17.6 MB (49.45%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,27 +1,31 @@
-from heapq import heappush, heappop
-from collections import defaultdict
+from heapq import heappop, heappush
+from typing import List
+
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        graph = defaultdict(list)
-        for a, b, price in flights:
-            graph[a].append((b, price))
         
-        node2minNstop = {i: float("inf") for i in range(n)}
-        pq = [(0, src, -1)]
+        # Build the graph
+        graph = [[] for _ in range(n)]
+        dis = [float('inf')] * n
+        
+        for flight in flights:
+            u, v, w = flight
+            graph[u].append((v, w))
+        
+        pq = deque()
+        pq.append((0, src, 0))  # dis, source, no of stops
+        
+        dis[src] = 0
+        
         while pq:
-            dist_from_src, node, n_stops = heappop(pq)
-            if node == dst:
-                return dist_from_src
-			# skip current iteration if number of stops exceeds maximum
-			# or we can reach current node with fewer stops. Suppose we've
-			# visited this node before, by the property of Dijkstra we must have found
-			# a path with lower cost to this node before. The only reason we might want
-			# to visit it again is if we can visit it with fewer stops. If this is not the case, we
-			# skip.
-            if n_stops >= min(k, node2minNstop[node]):
-                continue
-            node2minNstop[node] = n_stops
-            for neighbor, dist_to_neighbor in graph[node]:
-                heappush(pq, (dist_from_src+dist_to_neighbor, neighbor, n_stops+1))
-                    
-        return -1
+            d, u, st = pq.popleft()
+            
+            for v, w in graph[u]:
+                if d + w < dis[v] and st <= k:
+                    dis[v] = d + w
+                    pq.append((dis[v], v, st + 1))
+        
+        if dis[dst] == float('inf'):
+            return -1
+        
+        return dis[dst]
