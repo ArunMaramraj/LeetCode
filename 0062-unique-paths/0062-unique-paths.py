f26f9bf07f26aa2bca5ee4ea81cac3551a88cada
Arun Maramraj <imarun500@gmail.com>
2023-06-20 10:39:40 +0530

    Time: 50 ms (37.49%), Space: 16.4 MB (17.46%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -4,23 +4,42 @@ class Solution:
         
         dp = [[-1]*n for i in range(m)]
         
-        def compute(i,j):
+#         def compute(i,j):
             
-            if i<0 or j<0 :
-                return 0
+#             if i<0 or j<0 :
+#                 return 0
             
-            if i==0 and j==0:
-                return 1
+#             if i==0 and j==0:
+#                 return 1
             
-            if dp[i][j] != -1:
-                return dp[i][j]
+#             if dp[i][j] != -1:
+#                 return dp[i][j]
             
-            up = compute(i-1,j)
-            left = compute(i,j-1)
+#             up = compute(i-1,j)
+#             left = compute(i,j-1)
             
-            dp[i][j] = up + left
+#             dp[i][j] = up + left
             
-            return dp[i][j]
+#             return dp[i][j]
         
         
-        return compute(m-1,n-1)
+#         return compute(m-1,n-1)
+    
+    
+        for i in range(n):
+            dp[0][i] = 1
+        for j in range(m):
+            dp[j][0] = 1
+        
+        def compute():
+            
+            for row in range(1,m):
+                for col in range(1,n):
+                        dp[row][col] = dp[row-1][col] + dp[row][col-1]
+                        
+            return dp[m-1][n-1]
+        
+        return compute()
+    
+    
+    
